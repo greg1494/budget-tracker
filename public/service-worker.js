5ce7bc017e2a2387af94bdc917a0727521519c43
Greg Christiansen <gregchristiansen14@gmail.com>
2022-02-22 18:26:00 -0600

    issue with the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,21 @@
-const APP_PREFIX = 'budget-';     
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./js/index.html",
-    "./css/style.css",
-    "./js/index.js"
+  '/',
+  './index.html',
+  './manifest.json',
+  './css/styles.css',
+  './js/idb.js',
+  './js/index.js',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
 ];
 
 // Respond with cached resources
